eea722780afe7d0af0fc89990e90c2de43cea165
Eddie Harrison <theeddieharrison@gmail.com>
2019-10-09 14:21:56 -0700

    Shells out to pretty-print a list of aliases.

---
+++ git/aliases.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# this is pretty roundabout to gt the formatting right...
+print_git_aliases()  {
+    (   
+        echo "[ALIAS]" "[COMMAND]"
+        git config --global --get-regexp alias.* \
+            | sed 's/alias.//' \
+            | sed 's/ /X/g' \
+            | sed 's/X/ /'
+    ) \
+        | sort \
+        | column -t \
+        | sed 's/X/ /g'
+}
