8cc9d7995f97023b53e5af5e692b6884053773a7
Eddie Harrison <theeddieharrison@gmail.com>
2020-02-07 12:03:34 -0800

    Updates tele commands.

--- tele/aliases.sh
+++ tele/aliases.sh
@@ -2,15 +2,28 @@
 
 telin() {
     env=${1}
-    cluster=${2:-2}
-    tsh  logout --debug
-    tele logout --debug
-    tele login  --debug --auth=Keycloak --ops opscenter.${env}.msap.io stable${cluster} \
-                                                                         && tsh login --debug
+
+    if [[ "${2}" == "ops" ]] || [[ "${2}" == "hub" ]]; then
+        cluster="opscenter"
+    else
+        cluster="stable${2}"
+    fi
+
+    telout ${tele_cmd}
+
+    tele login \
+        --debug \
+        --auth Keycloak \
+        --hub opscenter.${env}.msap.io \
+        ${cluster}
+
+    # tsh login \
+    #     --debug \
+    #     --proxy opscenter.${env}.msap.io
 }
 
 telout() {
-    tsh  logout --debug
+    # tsh  logout --debug
     tele logout --debug
 }
 
@@ -20,11 +33,14 @@ alias kbuild-dev='telin kbuild-dev'
 alias cpdev='telin cpdev'
 alias kdev='telin kdev'
 alias kqa='telin kqa'
-alias kstg='telin kstg'
 
+alias kstg='telin kstg'
 alias kprod='telin kprod'
 alias kprod-eu='telin kprod-eu'
 
 alias kgbuild='telin gbuild'
 alias kgstg='telin kgstg'
 alias kgprod='telin kgprod'
+
+alias tele-env='tele status | grep "opscenter.[a-z-]*.msap.io" --only-matching | cut -d "." -f 2'
+alias tele-cluster='tele status | grep "Cluster" | cut -f 2'
