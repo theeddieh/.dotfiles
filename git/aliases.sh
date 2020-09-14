38c99ec7cc67ca23e488059156012aefc2d89e22
Eddie Harrison <theeddieharrison@gmail.com>
2020-09-14 12:19:04 -0700

    Adds a bunch of git helpers.

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# this is pretty roundabout to gt the formatting right...
+alias g=git
+alias h=hub
+
+# this is pretty roundabout to get the formatting right...
 print_git_aliases()  {
     (   
         echo "[ALIAS]" "[COMMAND]"
@@ -9,10 +12,43 @@ print_git_aliases()  {
             | sed 's/ /X/g' \
             | sed 's/X/ /'
     ) \
-        | sort \
         | column -t \
         | sed 's/X/ /g'
 }
 
-alias git=hub
-alias g=git
+list_status() {
+    # files=$(git status --short)
+    # for file in ${files[@]}; do
+    #     echo ${file}
+    # done
+
+    # lsst = !ls -aFGho $(
+    git status --short | awk {'printf(" %s \n", $1); printf("\n|%s|\n", $0)'}
+}
+
+refresh_master() {
+    branch=$(git symbolic-ref --short HEAD)
+    git checkout master
+    git fetch origin
+    git merge origin/master --ff-only
+    git checkout ${branch}
+}
+
+is_commit_signed() {
+    commit=${1:-"HEAD"}
+    git verify-commit ${commit} &> /dev/null
+    echo $?
+}
+
+list_signed_commits() {
+    num=${1:-"10"}
+    hashes=$(git log --pretty=format:'%h' -${num})
+
+    for hash in ${hashes[@]}; do
+        if [[ "$(is_commit_signed ${hash})" == "0" ]]; then
+            echo "🔐    ${hash}"
+        else
+            echo "⚠️    ${hash}"
+        fi
+    done
+}
